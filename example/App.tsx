6ec96890b8b25acf6ccf771f9121a8f079487aae
Gautham495 <kinggautham495@gmail.com>
2025-11-18 00:37:31 +0530

    chore: updated example

--- example/App.tsx
+++ example/App.tsx
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-
-import { encodeString, decodeToString } from 'react-native-craby-base64';
-
-function App() {
-  const [encoded, setEncoded] = useState<string>('');
-  const [decodedString, setDecodedString] = useState<string>('');
-
-  useEffect(() => {
-    const input = 'Hello World!';
-
-    // Encode
-    const b64 = encodeString(input);
-    setEncoded(b64);
-
-    // Decode (UTF-8)
-    const decoded = decodeToString(b64);
-    setDecodedString(decoded);
-  }, []);
-
-  return (
-    <View>
-      <Text>Encoded: {encoded}</Text>
-      <Text>Decoded String: {decodedString}</Text>
-    </View>
-  );
-}
-
-export default App;
-
+// import React, { useEffect, useState } from 'react';
 // import { Text, View } from 'react-native';
-// import React from 'react';
 
-// const App = () => {
+// import { encodeString, decodeToString } from 'react-native-craby-base64';
+
+// function App() {
+//   const [encoded, setEncoded] = useState<string>('');
+//   const [decodedString, setDecodedString] = useState<string>('');
+
+//   useEffect(() => {
+//     const input = 'Hello World!';
+
+//     // Encode
+//     const b64 = encodeString(input);
+//     setEncoded(b64);
+
+//     // Decode (UTF-8)
+//     const decoded = decodeToString(b64);
+//     setDecodedString(decoded);
+//   }, []);
+
 //   return (
 //     <View>
-//       <Text>App</Text>
+//       <Text>Encoded: {encoded}</Text>
+//       <Text>Decoded String: {decodedString}</Text>
 //     </View>
 //   );
-// };
+// }
 
 // export default App;
+
+import { Text, View } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
